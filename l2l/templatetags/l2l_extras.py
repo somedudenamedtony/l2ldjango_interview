7330119ebdfa1f2bd13239da166776d0c9342c0a
Tony Shepherd <anthony.shepherd@fbinv.com>
2023-08-24 22:04:07 -0600

    added tests and updated filter logic

--- l2l/templatetags/l2l_extras.py
+++ l2l/templatetags/l2l_extras.py
@@ -8,12 +8,12 @@ register = template.Library()
 def l2l_dt(value):
     dt_format = "%Y-%m-%d %H:%M:%S"
     dt_str_format = "%Y-%m-%dT%H:%M:%S"
-    dt = value
 
-    if not type(value) is str and not type(value) is datetime:
-        raise ValueError("Value must be a datetime or a string with the format " + dt_str_format)
+    if isinstance(value, datetime.datetime):
+        return value.strftime(dt_format)
 
-    if type(value) is str:
+    if isinstance(value, str):
         dt = datetime.datetime.strptime(value, dt_str_format)
+        return dt.strftime(dt_format)
 
-    return dt.strftime(dt_format)
+    raise ValueError("Value must be a datetime or a string with the format " + dt_str_format)
--- l2l/tests.py
+++ l2l/tests.py
@@ -1,3 +1,35 @@
+import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from l2l.templatetags.l2l_extras import l2l_dt
+
+
+class L2LDTFilterTestCase(TestCase):
+    # test valid string conversion
+    def test_valid_datetime_string(self):
+        input_str = "2023-08-24T15:30:00"
+        expected_output = "2023-08-24 15:30:00"
+        output = l2l_dt(input_str)
+        self.assertEqual(output, expected_output)
+
+    # test valid datetime conversion
+    def test_valid_datetime_object(self):
+        input_dt = datetime.datetime(2023, 8, 24, 15, 30, 0)
+        expected_output = "2023-08-24 15:30:00"
+        output = l2l_dt(input_dt)
+        self.assertEqual(output, expected_output)
+
+    def test_invalid_input_type(self):
+        # Int instead of datetime or string
+        with self.assertRaises(ValueError):
+            l2l_dt(123)
+
+        # Incorrect format
+        with self.assertRaises(ValueError):
+            l2l_dt("2023-08-24")
+
+    # Correct format but not ISO format
+    def test_invalid_datetime_string(self):
+        with self.assertRaises(ValueError):
+            l2l_dt("2023-08-24 15:30:00")
